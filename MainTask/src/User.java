b96ea93287215048977b38e5d779aaba323c12c6
hybridt <hokagemanns@gmail.com>
2024-12-21 16:46:31 +0500

    implement builder for user class

--- MainTask/src/User.java
+++ MainTask/src/User.java
@@ -1,9 +1,9 @@
 public class User {
-    private String name;
-    private String password;
-    private String email;
+    private final String name;
+    private final String password;
+    private final String email;
 
-    User(String name, String password, String email) {
+    private User(String name, String password, String email) {
         this.name = name;
         this.password = password;
         this.email = email;
@@ -13,24 +13,12 @@ public class User {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     @Override
@@ -40,5 +28,30 @@ public class User {
                 ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
                 '}';
+    }
+
+    public static class UserBuilder {
+        private String name;
+        private String password;
+        private String email;
+
+        public UserBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User build() {
+            return new User(name, password, email);
+        }
     }
 }
