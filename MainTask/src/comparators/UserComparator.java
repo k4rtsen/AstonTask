8aaa3c2a6e7362a22db71f3f0905b2a7475fd3e4
hybridt <hokagemanns@gmail.com>
2024-12-22 21:46:16 +0500

    implement comparator for user class

---
+++ MainTask/src/comparators/UserComparator.java
@@ -0,0 +1,32 @@
+package comparators;
+
+import models.User;
+
+import java.util.Comparator;
+
+public class UserComparator {
+
+    public static class ByName implements Comparator<User> {
+
+        @Override
+        public int compare(User user1, User user2) {
+            return user1.getName().compareTo(user2.getName());
+        }
+    }
+
+    public static class ByPassword implements Comparator<User> {
+
+        @Override
+        public int compare(User user1, User user2) {
+            return user1.getPassword().compareTo(user2.getPassword());
+        }
+    }
+
+    public static class ByEmail implements Comparator<User> {
+
+        @Override
+        public int compare(User user1, User user2) {
+            return user1.getEmail().compareTo(user2.getEmail());
+        }
+    }
+}
