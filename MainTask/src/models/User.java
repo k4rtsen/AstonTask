f8c0bfda290f54743d5d0579c7c563547e3f3109
Yuliya Kosenko <yulkosenko@ozon.ru>
2024-12-22 16:54:46 +0300

    moved sort models to separate folder and returned builders

---
+++ MainTask/src/models/User.java
@@ -0,0 +1,59 @@
+package models;
+
+public class User {
+    private final String name;
+    private final String password;
+    private final String email;
+
+    private User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static class UserBuilder {
+        private String name;
+        private String password;
+        private String email;
+
+        public UserBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User build() {
+            return new User(name, password, email);
+        }
+    }
+}
