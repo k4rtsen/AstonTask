451797f9fd07f9ab2e4e2780994b2c06a6c27ae8
Yuliya Kosenko <yulkosenko@ozon.ru>
2024-12-26 14:11:42 +0300

    added base actions with models implementations

---
+++ MainTask/src/actions/BusActions.java
@@ -0,0 +1,84 @@
+package actions;
+
+import filling.ByManual;
+import models.Bus;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+
+import static filling.ByManual.readInt;
+import static filling.ByManual.readString;
+import static utilities.FileUtilities.readFile;
+import static utilities.RandomUtilities.getRandomFromEnum;
+import static utilities.RandomUtilities.getRandomNumber;
+
+public class BusActions implements Actions<Bus> {
+    private static final Bus.BusBuilder busBuilder = new Bus.BusBuilder();
+
+    @Override
+    public String getModelName() {
+        return "Bus";
+    }
+
+    /**
+     * Метод заполнения массива User из файла input_User.txt
+     * @return массив List User
+     */
+    @Override
+    public List<Bus> fillByFile() {
+        String fileName = ".\\MainTask\\input_Bus.txt";
+        //TODO add checkers for fields
+        return readFile(fileName, (data, _) -> busBuilder
+                .setNumber(Integer.parseInt(data[0]))
+                .setModel(data[1])
+                .setMileage(Integer.parseInt(data[2]))
+                .build()
+        );
+    }
+
+    /**
+     * Метод заполнения массива Bus ручным способом
+     * @return массив List Bus
+     */
+    @Override
+    public List<Bus> fillManual() {
+        return ByManual.fillManual(getModelName(), (i) -> {
+            int number = readInt(String.format("%s[%d] - Введите номер автобуса (0 - отмена): ", getModelName(), i));
+            if (number == 0) return null;
+
+            String model = readString(String.format("%s[%d] - Введите модель автобуса (0 - отмена): ", getModelName(), i));
+            if (model.equals("0")) return null;
+
+            int mileage = readInt(String.format("%s[%d] - Введите пробег автобуса (0 - отмена): ", getModelName(), i));
+            if (mileage == 0) return null;
+
+            return busBuilder.setNumber(number)
+                    .setModel(model)
+                    .setMileage(mileage)
+                    .build();
+        });
+    }
+
+    /**
+     * Метод заполнения массива Bus случайным образом (рандом)
+     * @return массив List Bus
+     */
+    @Override
+    public List<Bus> fillRandom() {
+        int arraySize = readInt(String.format("Введите размер массива %s (0 - возврат назад): ", getModelName()));
+        if (arraySize == 0) return null;
+
+        List<Bus> models = new ArrayList<>(arraySize);
+
+        for (int i = 0; i < arraySize; i++) {
+            models.add(busBuilder
+                    .setNumber(getRandomNumber(10000, 99999))
+                    .setModel(getRandomFromEnum(getModelName()))
+                    .setMileage(getRandomNumber(10, 999999))
+                    .build());
+        }
+        return models;
+    }
+}
---
+++ MainTask/src/actions/StudentActions.java
@@ -0,0 +1,84 @@
+package actions;
+
+import filling.ByManual;
+import models.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static filling.ByManual.*;
+import static utilities.FileUtilities.readFile;
+import static utilities.RandomUtilities.*;
+import static utilities.RandomUtilities.getRandomAverageScore;
+
+public class StudentActions implements Actions<Student> {
+    private static final models.Student.StudentBuilder studentBuilder = new Student.StudentBuilder();
+
+    @Override
+    public String getModelName() {
+        return "Student";
+    }
+
+    /**
+     * Метод заполнения массива Student из файла input_Student.txt
+     * @return массив List Student
+     */
+    @Override
+    public List<Student> fillByFile() {
+        String fileName = ".\\MainTask\\input_Student.txt";
+        return readFile(fileName, (data, _) -> studentBuilder
+                .setGroup(data[0])
+                .setScore(Double.parseDouble(data[1]))
+                .setGradeBookNum(Integer.parseInt(data[2]))
+                .build()
+        );
+    }
+
+    /**
+     * Метод заполнения массива Student ручным способом
+     * @return массив List Student
+     */
+    @Override
+    public List<Student> fillManual() {
+        return ByManual.fillManual(getModelName(), (i) -> {
+            String group = readString(String.format("%s[%d] - Введите группу студента (0 - отмена): ",
+                    getModelName(), i));
+            if (group.equals("0")) return null;
+            double averageScore = readDouble(String.format("%s[%d] - Введите средний балл студента (0 - отмена): ",
+                    getModelName(), i));
+            // TODO is it correct?
+            if (averageScore == 0) return null;
+
+            int gradeBook = readInt(String.format("%s[%d] - Введите номер зачетной книжки студента (0 - отмена): ",
+                    getModelName(), i));
+            if (gradeBook == 0) return null;
+
+            return studentBuilder
+                    .setGroup(group)
+                    .setScore(averageScore)
+                    .setGradeBookNum(gradeBook)
+                    .build();
+        });
+    }
+
+    /**
+     * Метод заполнения массива Student случайным образом (рандом)
+     * @return массив List Student
+     */
+    @Override
+    public List<Student> fillRandom() {
+        int arraySize = readInt(String.format("Введите размер массива %s (0 - возврат назад): ", getModelName()));
+        if (arraySize == 0) return null;
+
+        List<Student> models = new ArrayList<>(arraySize);
+
+        for (int i = 0; i < arraySize; i++) {
+            models.add(studentBuilder
+                    .setGroup(getRandomStudentGroup())
+                    .setScore(getRandomAverageScore(1, 10))
+                    .setGradeBookNum(getRandomNumber(10000, 99999))
+                    .build());
+        }
+        return models;
+    }
+}
